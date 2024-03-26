611e8321f9580bbb9b24f8d39649b30c5fe7d5e1
Tainá Santana <taina.santana1305@gmail.com>
2024-03-26 11:19:18 -0300

    Aula 07 - Utilizando React Fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import Frase from './components/Frase'
+import List from './components/List'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <SayMyName nome="Tai" />
       <SayMyName nome={name} />
       <Pessoa  nome = 'Rodrigo' idade='28' profissao = 'Programador' foto= 'https:via.placeholder.com/150'/>
+      <List />
     </div>
   );
 }
