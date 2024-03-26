b7eea21c59d60a2ce86290bbf0d2ce6f75cdb653
Tainá Santana <taina.santana1305@gmail.com>
2024-03-26 09:27:34 -0300

    Trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld'
 import Titulo from './components/Titulo'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
   const name = "Tainá";
-  const newName = name.toUpperCase();
 
   const url = 'https://via.placeholder.com/150';
 
-  function sum(a, b) {
-    return a + b;
-  }
-
   return (
     <div className='App'>
-      <h2>Alteração no jsx</h2>
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="minha imagem" /> {/**trabalhando com jsx */} 
-      <HelloWorld /> {/**trabalhando com componentes */}
-      <Titulo/>
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="Tai" />
+      <SayMyName nome={name} />
+      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https:via.placeholder.com/150"/>
     </div>
   );
 }
