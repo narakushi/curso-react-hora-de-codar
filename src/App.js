0e08539336bb65cf5501b83b97c2fd950ab8880f
Tainá Santana <taina.santana1305@gmail.com>
2024-03-26 11:00:29 -0300

    aula 06 - inserindo css no react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
+
 
 function App() {
   const name = "Tainá";
   return (
     <div className='App'>
+      <Frase />
+      <Frase />
+      <h1>Testando css</h1>
       <SayMyName nome="Matheus" />
       <SayMyName nome="Tai" />
       <SayMyName nome={name} />
