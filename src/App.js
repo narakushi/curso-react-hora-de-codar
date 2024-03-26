7fe87b379d8d9df319d09f2327df5a4a0130f1a2
Tainá Santana <taina.santana1305@gmail.com>
2024-03-26 08:36:27 -0300

    trabalhando com componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
 
 function App() {
+  const name = "Tainá";
+  const newName = name.toUpperCase();
+
+  const url = 'https://via.placeholder.com/150';
+
+  function sum(a, b) {
+    return a + b;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>Alteração no jsx</h2>
+      <p>Olá, {newName}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="minha imagem" />
+      <HelloWorld />
     </div>
   );
 }
